c0e6fb4e1284e2e64c696a8707073a7c9661e579
Melo-Lucas <lucs.r06@gmail.com>
2021-08-18 10:33:13 -0300

    script que soluciona problema de conversão da tarefa de funções

--- conversao.py
+++ conversao.py
@@ -1,2 +1,10 @@
 # conversão de temperatura em Fahrenheit para Celsius
-# recebe como entrada o valor da temperatura em valor 
+# ENTRADA: recebe como entrada o valor da temperatura em float
+# SAÍDA: imprimir o valor convertido de Fahrenheit para Celsius. O valor convertido deve ser apresentado com somente uma casa decimal
+
+def converte():
+    fahrenheit = float(input())
+    celsius = ((fahrenheit - 32)*5/9)
+    print(round(celsius, 1))
+
+converte()

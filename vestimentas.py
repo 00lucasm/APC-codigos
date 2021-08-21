2af8024eb3e95d6a71ca4e54d10109d85a13213e
Melo-Lucas <lucs.r06@gmail.com>
2021-08-21 18:13:21 -0300

    solução do problema vestimentas v1

--- vestimentas.py
+++ vestimentas.py
@@ -1,1 +1,12 @@
 # to do...
+def vestimentas(x_botas, y_chapeus):
+    if(x_botas >= y_chapeus):
+        print(y_chapeus)
+    else:
+        print(x_botas)
+'''
+x = int(input())
+y = int(input())
+
+vestimentas(x, y)
+'''

d4a2e8cb99a10e2cf0a474e7f84bdb3922488407
Melo-Lucas <lucs.r06@gmail.com>
2021-08-22 13:28:54 -0300

    resolução vestimentas part2

---
+++ vestimentas2.py
@@ -0,0 +1,10 @@
+'''
+Entrada: valores inteiros x e y, com x correspondendo à quantidade de pares de botas
+e y correspondendo à quantidade de chapéus
+
+Saída: quantidade máxima de peças nos jogos de vestimentas
+'''
+
+def vestimentas(x_pares, y_chapeus):
+    
+    return None

7ffa12b693d60b49b0d257c82962132dcba90046
Melo-Lucas <lucs.r06@gmail.com>
2021-08-22 14:03:53 -0300

    solução final para o problema

--- vestimentas2.py
+++ vestimentas2.py
@@ -4,7 +4,14 @@ e y correspondendo à quantidade de chapéus
 
 Saída: quantidade máxima de peças nos jogos de vestimentas
 '''
+def vestimentas(x_botas, y_chapeus):
+    if(x_botas >= y_chapeus):
+        print(y_chapeus*2)
+    else:
+        print(x_botas*2)
 
-def vestimentas(x_pares, y_chapeus):
-    
-    return None
+# TESTES:
+#x = int(input())
+#y = int(input())
+
+#vestimentas(x, y)

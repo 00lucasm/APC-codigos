1a1ee7809f22fb9a66113dd9b856780eb052b2c2
Melo-Lucas <lucs.r06@gmail.com>
2021-08-29 23:01:34 -0300

    condicionais e recursividade

---
+++ realidade.py
@@ -0,0 +1,6 @@
+def realidade(a, b, c):
+    delta = ((b**2) - 4*a*c)
+    if(delta >= 0):
+        print("reais")
+    else:
+        print("complexas")
---
+++ uri1061.cpp
@@ -0,0 +1,11 @@
+// tempo de um evento "famooooso"
+#include <iostream>
+#include <cstring>
+#include <string>
+
+int main(){
+
+    
+
+    return 0;
+}

aa286f1c796400e0fdc1408284141578308cd87a
Melo-Lucas <lucs.r06@gmail.com>
2021-09-22 18:41:44 -0300

    até neste commit são feitas funções para identificar palíndromos

--- Strings/palindromo.py
+++ Strings/palindromo.py
@@ -1,13 +1,28 @@
+#!/bin/python3
 """
 STRINGS == cadeia de caracteres!
 """
-
-def palindromo(frase):
+# essa função só funciona para strings
+def palindromo_string(frase):
     return frase == frase[::-1]
 
-# Início do programa
+def palindromo(frase):
+    frase = frase.replace(' ','')
+    return frase.lower() == frase[::-1].lower()
+
+# Conta quantas vezes string ocorre em frase
+def busca_string(frase, string):
+    cont = 0
+    i = -1
+    while True:
+        i = frase.find(string, i + 1)
+        if i == -1:
+            return cont
+        else:
+            cont += 1
 
 frase = input("Digite uma frase: ")
 # palavra = input("Digite uma palavra: ")
 
-print(f'É palíndromo? {palindromo(frase)}')
+print(f'É uma string palíndromo? {palindromo_string(frase)}')
+print(f'É uma frase palíndromo? {palindromo(frase)}')
